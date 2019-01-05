5e893f9cdfa4713407aad66878493a08574e8fe8
hatf0 <harrison@malw.io>
2019-01-05 14:31:45 -0500

    Revert "Create api_keys.rb"
    This reverts commit c723c92410bc37134a68e7e199b395d6851aa34a.

--- api_keys.rb
+++
@@ -1,4 +0,0 @@
-$calendarID = '' # Calendar ID. This should be an email or 'primary'.
-$last_fm_api_key = '' # last.fm API Key. You can get this pretty easily.
-$weather_api_key = '' # OpenWeatherMap API Key
-$airnow_api_key = '' # AirNow.gov api key
