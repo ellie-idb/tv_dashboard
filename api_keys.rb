c723c92410bc37134a68e7e199b395d6851aa34a
hatf0 <harrison@0xcc.pw>
2019-01-05 14:31:03 -0500

    Create api_keys.rb

---
+++ api_keys.rb
@@ -0,0 +1,4 @@
+$calendarID = '' # Calendar ID. This should be an email or 'primary'.
+$last_fm_api_key = '' # last.fm API Key. You can get this pretty easily.
+$weather_api_key = '' # OpenWeatherMap API Key
+$airnow_api_key = '' # AirNow.gov api key
